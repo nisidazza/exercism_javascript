12a0621af4d90df1972378cd9d0ebf040e0aaef3
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-02-02 15:52:21 +1300

    working on matrix.js solution

--- javascript/matrix/matrix.js
+++ javascript/matrix/matrix.js
@@ -4,15 +4,26 @@
 //
 
 export class Matrix {
-  constructor() {
-    throw new Error("Remove this statement and implement this function");
+  constructor(matrix) {
+    this.matrixArr = matrix.split('\n')  
+      .map(row => row.split(' ')
+        .map(Number)) // Calls Number on each value in the array (casting it to a number) and returns the array of results
   }
 
   get rows() {
-    throw new Error("Remove this statement and implement this function");
+   return this.matrixArr
   }
 
   get columns() {
-    throw new Error("Remove this statement and implement this function");
+    let getColumns = this.matrixArr[0]
+      .map((col, index) => this.matrixArr
+        .map(row => row[index])
+      )
+    return getColumns
   }
+
 }
+
+new Matrix('1 2 3 4\n5 6 7 8\n9 8 7 6').columns[3]
+
+

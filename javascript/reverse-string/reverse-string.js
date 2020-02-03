03c42825ad092dff3023ad02858c26bc32b3495e
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-02-03 17:15:19 +1300

    solution decrementing For Loop

--- javascript/reverse-string/reverse-string.js
+++ javascript/reverse-string/reverse-string.js
@@ -1,4 +1,13 @@
-
-export const reverseString = () => {
-  throw new Error("Remove this statement and implement this function");
+//-- SOLUTION 1 --
+export const reverseString = string => {
+  let reverseStr = "";
+  for (let i = string.length - 1; i >= 0; i--) {
+    reverseStr += string[i];
+  }
+  return reverseStr;
 };
+
+let str = 'a word'
+reverseString(str)
+
+

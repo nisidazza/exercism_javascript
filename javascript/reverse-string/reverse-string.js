3804a32b0a50b3ba2e5afa84614e2fec7d0d0180
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-02-03 17:16:25 +1300

    solution using Built-in Method

--- javascript/reverse-string/reverse-string.js
+++ javascript/reverse-string/reverse-string.js
@@ -1,13 +1,22 @@
 //-- SOLUTION 1 --
+// export const reverseString = string => {
+//   let reverseStr = "";
+//   for (let i = string.length - 1; i >= 0; i--) {
+//     reverseStr += string[i];
+//   }
+//   return reverseStr;
+// };
+
+// let str = 'a word'
+// reverseString(str)
+
+// -- SOLUTION 2 --
 export const reverseString = string => {
-  let reverseStr = "";
-  for (let i = string.length - 1; i >= 0; i--) {
-    reverseStr += string[i];
-  }
-  return reverseStr;
+  return string
+    .split("")
+    .reverse()
+    .join("");
 };
 
-let str = 'a word'
-reverseString(str)
-
-
+let s = "a capitalized word";
+reverseString(s);

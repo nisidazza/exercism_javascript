68d8042f3bd3e6ad655a6985d2b6e63a257ff412
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-02-03 19:21:08 +1300

    while loop solution

--- javascript/collatz-conjecture/collatz-conjecture.js
+++ javascript/collatz-conjecture/collatz-conjecture.js
@@ -3,6 +3,18 @@
 // convenience to get you started writing code faster.
 //
 
-export const steps = () => {
-  throw new Error("Remove this statement and implement this function");
+export const steps = n => {
+  let count = 0;
+  if (n === 0 || n < 0) throw new Error("Only positive numbers are allowed");
+  while (n > 0 && n !== 1) {
+    if (n % 2 === 0) {
+      n = n / 2;
+      count++;
+    } else {
+      n = n * 3 + 1;
+      count++;
+    }
+  }
+
+  return count;
 };

3115b19a9637807e6bf8dab3838ac5c8a979ecb0
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-02-03 13:05:00 +1300

    review and submit resistor-color solution
    approve by mentor

--- javascript/resistor-color/resistor-color.js
+++ javascript/resistor-color/resistor-color.js
@@ -1,18 +1,27 @@
-export const COLORS = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"];
-
+export const COLORS = [
+  "black",
+  "brown",
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "violet",
+  "grey",
+  "white"
+];
 
 // -- SOLUTION 1 --
-export const colorCode = (color) => {
-  let i = 0
-  while (i < COLORS.length) {
-    if (color == COLORS[i]) {
-      return i
-    }
-    i++
-  }
-  return -1
-};
-
+// export const colorCode = color => {
+//   let i = 0;
+//   while (i < COLORS.length) {
+//     if (color == COLORS[i]) {
+//       return i;
+//     }
+//     i++;
+//   }
+//   return -1;
+// };
 
 // -- SOLUTION 2 --
 // export const colorCode = (color) => {
@@ -20,5 +29,4 @@ export const colorCode = (color) => {
 //   return number
 // };
 
-
-
+export const colorCode = color => COLORS.indexOf(color);

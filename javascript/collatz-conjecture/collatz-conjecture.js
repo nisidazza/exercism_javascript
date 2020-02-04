aeeb8c59b9bb36dc27b813919032c4616e2d56f1
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-02-04 13:59:31 +1300

    refactoring collatz-conjecture.js

--- javascript/collatz-conjecture/collatz-conjecture.js
+++ javascript/collatz-conjecture/collatz-conjecture.js
@@ -1,20 +1,17 @@
-//
-// This is only a SKELETON file for the 'Collatz Conjecture' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 export const steps = n => {
   let count = 0;
-  if (n === 0 || n < 0) throw new Error("Only positive numbers are allowed");
-  while (n > 0 && n !== 1) {
-    if (n % 2 === 0) {
-      n = n / 2;
-      count++;
-    } else {
-      n = n * 3 + 1;
-      count++;
-    }
-  }
+  if (n <= 0) throw new Error("Only positive numbers are allowed");
 
+  while (n !== 1) {
+    //   if (n % 2 === 0) {
+    //     n = n / 2;
+    //   } else {
+    //     n = n * 3 + 1;
+    //   }
+
+    // --SOLUTION USING TERNARY OPERATOR--
+    n = n % 2 === 0 ? n / 2 : n * 3 + 1;
+    count++;
+  }
   return count;
 };

5a567ac8f5d336d9444bf0c14e6b930aacc8ae96
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-02-07 16:28:34 +1300

    working on "word-count" solution

--- javascript/word-count/word-count.js
+++ javascript/word-count/word-count.js
@@ -1,8 +1,44 @@
-//
-// This is only a SKELETON file for the 'Word Count' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
-export const countWords = () => {
-  throw new Error("Remove this statement and implement this function");
+export const countWords = phrase => {
+  let cleanedPhrase = phrase
+    .trim()
+    .toLowerCase()
+    .replace(/[./#!$%^&*;:{}=\-_`~()@?¡¿,\u0091]/g, " ")
+    .replace(/\s+/g, "||");
+  let wordsArr = cleanedPhrase.split("||");
+  //console.log(wordsArr);
+  let objWordsCount = {};
+  for (let i = 0; i < wordsArr.length; i++) {
+    let word = wordsArr[i];
+    //console.log(word);
+    if (word !== "") {
+      if (!objWordsCount[word]) {
+        objWordsCount[word] = 1;
+      } else {
+        objWordsCount[word]++;
+      }
+    }
+  }
+  return objWordsCount;
 };
+
+// export const countWords = phrase => {
+//   let map = {};
+//   phrase
+//     .trim()
+//     .toLowerCase()
+//     .split(/[ ,\n]+/g)
+//     .forEach(element => {
+//       element = element.replace(/[.,!:"&@$%^]|^'|'$/g, "");
+//       if (element) {
+//         if (Object.prototype.hasOwnProperty.call(map, element)) {
+//           map[element]++;
+//         } else {
+//           map[element] = 1;
+//         }
+//       }
+//     });
+//   return map;
+// };
+
+let s = "car: carpet as java: javascript!!&@$%^&";
+console.log(countWords(s));

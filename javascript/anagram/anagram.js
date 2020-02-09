54c1febedd208657f2e055e09e81b6359f01badf
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-02-09 15:57:30 +1300

    pass tests - anagram

--- javascript/anagram/anagram.js
+++ javascript/anagram/anagram.js
@@ -3,6 +3,37 @@
 // convenience to get you started writing code faster.
 //
 
-export const findAnagrams = () => {
-  throw new Error("Remove this statement and implement this function");
-}
+export const findAnagrams = (mainWord, wordsArr) => {
+  let sublist = [];
+  for (let i = 0; i < wordsArr.length; i++) {
+    let word = wordsArr[i];
+    if (mainWord.length !== word.length) {
+      continue;
+    } else {
+      if (checkLetters(mainWord, word)) {
+        sublist.push(word);
+      }
+    }
+  }
+  return sublist;
+};
+
+const checkLetters = (mainWord, word) => {
+  let tempMainWord = mainWord.toLowerCase();
+  let tempWord = word.toLowerCase();
+  if(tempMainWord === tempWord) {
+    return false
+  }
+  for (let i = 0; i < tempWord.length; i++) {
+    if (tempMainWord.includes(tempWord[i])) {      
+      tempMainWord = tempMainWord.replace(tempWord[i], '')
+    }else {
+      return false
+    }
+  }
+  return true;
+};
+
+let x = "master";
+let y = ["stream", "pigeon", "maters"];
+findAnagrams(x, y);

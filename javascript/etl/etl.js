dd2d6577d76004c792f219a3ec5d1a3d5ef3e876
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-02-09 13:20:51 +1300

    download etl exercise
    install npm and bebl-node
    add debug config

---
+++ javascript/etl/etl.js
@@ -0,0 +1,18 @@
+//
+// This is only a SKELETON file for the 'etl' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+const oldSystem = {
+  1: ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],
+  2: ["D", "G"],
+  3: ["B", "C", "M", "P"],
+  4: ["F", "H", "V", "W", "Y"],
+  5: ["K"],
+  8: ["J", "X"],
+  10: ["Q", "Z"]
+};
+
+export const transform = () => {};
+
+transform()

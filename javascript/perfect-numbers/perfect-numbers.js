ae7d67e222b7a6ce05697d3c79cf784ebf91779e
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-02-09 13:18:47 +1300

    pass tests - perfect number

--- javascript/perfect-numbers/perfect-numbers.js
+++ javascript/perfect-numbers/perfect-numbers.js
@@ -3,6 +3,26 @@
 // convenience to get you started writing code faster.
 //
 
-export const classify = () => {
-  throw new Error("Remove this statement and implement this function");
+export const classify = num => {
+  let aliquotSum = calculateFactorSum(num);
+  if (num <= 0) {
+    throw new Error("Classification is only possible for natural numbers.");
+  }
+  if (aliquotSum == num) {
+    return "perfect";
+  } else if (aliquotSum > num) {
+    return "abundant";
+  } else if (aliquotSum < num) {
+    return "deficient";
+  }
+};
+
+const calculateFactorSum = n => {
+  let sum = 0;
+  for (let i = 0; i < n; i++) {
+    if (n % i == 0) {
+      sum += i;
+    }
+  }
+  return sum;
 };
